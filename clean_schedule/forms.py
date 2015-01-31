d0fa18607f6336d828fcbd8f129362656300a0dc
Michael Parrott <michael.parrott94@gmail.com>
2015-01-31 01:07:58 -0500

    Form stuff

--- clean_schedule/forms.py
+++ clean_schedule/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class SignUpForm(forms.Form):
   user_name = forms.CharField(label='Your name', max_length=100)
+  email = forms.CharField(label='Your email', max_length=100)
   password = forms.CharField(label='Your password', max_length=100)
