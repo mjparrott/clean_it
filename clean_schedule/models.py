be841bb9771a724b4baa874a3b7c7ab68a21bde2
Michael Parrott <michael.parrott94@gmail.com>
2015-01-30 22:53:29 -0500

    Start working on models

--- clean_schedule/models.py
+++ clean_schedule/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=100)
+
+class Group(models.Model):
+  name = models.CharField(max_length=100)
+  users = models.ManyToManyField(User)
+
