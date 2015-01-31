6bdee07b58399432a381edd4886760ef358f1844
Michael Parrott <michael.parrott94@gmail.com>
2015-01-30 23:55:38 -0500

    Add templates

--- clean_schedule/views.py
+++ clean_schedule/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Test")
+  template = loader.get_template('clean_schedule/index.html')
+  context = RequestContext(request, {})
+  return HttpResponse(template.render(context))
