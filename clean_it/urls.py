455e974d71060a6d2e73590f08dfcd9654b081a3
Michael Parrott <michael.parrott94@gmail.com>
2015-01-31 05:04:03 -0500

    Clean up URLs

--- clean_it/urls.py
+++ clean_it/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-  url(r'^admin/', include(admin.site.urls)),
-  url(r'^clean_schedule/*', include('clean_schedule.urls')),
+  url(r'^/*', include('clean_schedule.urls')),
 )
