a38315842a227045778214e8ea7c91202327c5c8
Michael Parrott <michael.parrott94@gmail.com>
2015-01-31 05:05:09 -0500

    Improve on group features.

---
+++ clean_schedule/actions.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+from clean_schedule import forms
+from clean_schedule.models import Group, CleanUser
+
+def log_out(request):
+  logout(request)
+  return HttpResponseRedirect('/clean_schedule')
--- clean_schedule/urls.py
+++ clean_schedule/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
 
-from clean_schedule import views
+from clean_schedule import actions, views
 
 urlpatterns = patterns('',
   url(r'^groups/create_group', views.create_group, name='create_group'),
   url(r'^sign_up', views.sign_up, name='sign_up'),
   url(r'^log_in', views.log_in, name='log_in'),
+  url(r'^log_out', actions.log_out, name='log_out'),
   url(r'^$', views.index, name='index'),
 )
 
